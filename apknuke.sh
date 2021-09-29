7449e2921077f8c1da97561b312a03e7ec5c61ed
name <name@hh.com>
2021-09-29 17:53:02 +0300

    update

--- apknuke.sh
+++ apknuke.sh
@@ -1,4 +1,4 @@
-PATH_TO_NucleiTemplates="/[path]/mobile-nuclei-templates/"
+PATH_TO_NucleiTemplates="./mobile-nuclei-templates/"
 
 target="$1" 
 
@@ -12,13 +12,4 @@ echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Running Nuclei Templates ... \e[0m\n"
 
 mkdir "$target"_nuclei_output
 
-pwd | (read foo; echo "$foo"/"$target"_decompile | nuclei --silent -t $PATH_TO_NucleiTemplates -o "$target"_nuclei_output/nuk.txt )
-
-if [[ $(cat ./"$target"_nuclei_output/nuk.txt | wc -l) -le 1 ]]; then
-    echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m No results ... Better Luck Next time \e[0m\n" 
-  else echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Results Saved in "$target"_nuclei_output/ \e[0m\n" 
-cat "$target"_nuclei_output/nuk.txt | grep "info" | sort -u >> "$target"_nuclei_output/info.txt
-cat "$target"_nuclei_output/nuk.txt | grep -v "info" | sort -u >> "$target"_nuclei_output/non-info.txt
-cat "$target"_nuclei_output/nuk.txt | grep "firebase-database" | sort -u >> "$target"_nuclei_output/non-info.txt
-rm "$target"_nuclei_output/nuk.txt
-fi
+pwd | (read foo; echo "$foo"/"$target"_decompile | nuclei -silent -json -t $PATH_TO_NucleiTemplates -o output.txt )
